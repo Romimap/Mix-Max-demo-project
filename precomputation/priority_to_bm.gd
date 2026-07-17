77b81186b25d5c02579d4ed40974b5f8a55af217
Romimap <romimap75@gmail.com>
2026-07-17 12:17:40 +0200

    tests and comments

--- precomputation/priority_to_bm.gd
+++ precomputation/priority_to_bm.gd
@@ -5,7 +5,7 @@ class_name PriorityToBM extends Node
 @export var priority_map : Texture2D
 @export var export_path : String = "res://textures/BM.exr"
 
-
+# Precondition : img must be of size 1x1 or more
 func get_mean(img : Image) -> Color:
 	var mean = Color(0, 0, 0, 0)
 	var H = img.get_height()
@@ -16,16 +16,22 @@ func get_mean(img : Image) -> Color:
 			mean += img.get_pixel(x, y);
 	return mean / N
 
-func fill_centered_priorities(img_in, img_out) -> void:
+
+# Precondition : img_in and img_out must be of the same size
+func fill_centered_priorities(img_in : Image, img_out : Image) -> void:
 	var mean_priority = get_mean(img_in).r
+	var H = img_in.get_height()
+	var W = img_in.get_width()
 	
-	for y in img_in.get_height():
-		for x in img_in.get_width():
+	for y in H:
+		for x in W:
 			var in_pixel : Color = img_in.get_pixel(x, y)
-			var out_pixel : Color = img_out.get_pixel(x, y)
+			var out_pixel : Color = Color.BLACK
+			
 			var centered_priority : float = in_pixel.r - mean_priority;
 			out_pixel.r = centered_priority
 			out_pixel.g = centered_priority * centered_priority
+			
 			img_out.set_pixel(x, y, out_pixel)
 
 
